77e5fcb81cb5c21f3756d9656a9c7ca1a7ff922c
Priyanka Shaw <priyanka12kol@gmail.com>
2025-07-15 11:35:27 +0000

    commit:15Jul

--- macros/macro.sql
+++ macros/macro.sql
@@ -6,3 +6,11 @@
     {{ return( (numerator * 100) / denominator ) }}
   {% endif %}
 {% endmacro %}
+
+{% macro convert_currency(value,currency) %}
+    {% if currency=='eur'%}
+        {{return (value*86/100)}}
+    {% else %}
+    {{ return (value*147) }}
+  {% endif %}
+{% endmacro %}
